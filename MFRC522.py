80fd91bc9341c7c9757e3f750e276d4c45fa6910
Bracken Dawson <abdawson@gmail.com>
2015-02-04 18:32:25 +0000

    add write block method

--- MFRC522.py
+++ MFRC522.py
@@ -403,6 +403,31 @@ class MFRC522:
         if status == self.MI_OK:
             print "Data written"
 
+  def Write_Block(self, blockAddr, writeData):
+    buff = []
+    buff.append(self.PICC_WRITE)
+    buff.append(blockAddr)
+    crc = self.CalulateCRC(buff)
+    buff.append(crc[0])
+    buff.append(crc[1])
+    (status, backData, backLen) = self.MFRC522_ToCard(self.PCD_TRANSCEIVE, buff)
+    if not(status == self.MI_OK) or not(backLen == 4) or not((backData[0] & 0x0F) == 0x0A):
+        status = self.MI_ERR
+    if status == self.MI_OK:
+        i = 0
+        buf = []
+        while i < 16:
+            buf.append(writeData[i])
+            i = i + 1
+        crc = self.CalulateCRC(buf)
+        buf.append(crc[0])
+        buf.append(crc[1])
+        (status, backData, backLen) = self.MFRC522_ToCard(self.PCD_TRANSCEIVE,buf)
+        if not(backLen == 4) or not((backData[0] & 0x0F) == 0x0A):
+          status = self.MI_ERR
+        return status, backData
+        
+
   def MFRC522_DumpClassic1K(self, key, uid):
     i = 0
     while i < 64:
